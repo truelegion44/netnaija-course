2a1876d1eee7ca7be9538ebc58abb357a6071cdf
nathanielchebem@gmail.com <nathanielchebem@gmail.com>
2023-08-05 00:41:31 -0700

    new

---
+++ controlflow.js
@@ -0,0 +1,31 @@
+// for loops
+
+//for(let i = 0; i<6; i++){
+//console.log('in loop:', i)} 
+//console.log('loop finished  ');
+
+// while loops 
+//let i = 0;
+//while( i < 6){
+//console.log('in loop:', i);
+//i++;} 
+
+
+//const names = ['shaun', 'bella', 'joy'];
+
+//let i = 0;
+//while (i < names.length) {
+ // console.log(names[i]);
+ // i++;}
+
+
+//do while loops
+
+let i =5;
+
+do{
+  console.log('val of i is: ', i);
+  i++;
+} while( i < 5) {
+  
+}
